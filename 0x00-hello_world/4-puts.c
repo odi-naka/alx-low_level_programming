64c20245b554e5d19d5a391306b087ab13e67ea1
odi-naka <odyobasi@gmail.com>
2022-09-08 18:20:08 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
 /**
-*main- print the string in the put function
-*
-*Description: using the main function
-*This program prints “programing is like building multilingual puzzle
-*Return: 0
+ * main - Entry point
+ *
+ * Return: Always 0(Success)
 */
 int main(void)
 {
-       puts("\"Programming is like building a multilingual puzzle");
-       return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-^c
-root@eb5466075fab:/alx-low_level_programming/0x00-hello_world# chmod u+x 4-puts.c
-
