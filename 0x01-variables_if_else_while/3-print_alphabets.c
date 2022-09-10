0cbea3aff52c8d686311f4c0240757a26a200bc4
odi-naka <odyobasi@gmail.com>
2022-09-09 18:27:00 -0700

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,15 +7,13 @@
  */
 int main(void)
 {
-	char letter ;
-
+	char letter;
 	
 	for (letter = 'a' ; letter <= 'z'; letter++)
 		putchar(letter);
 
 	for (letter = 'A'; leter <= 'Z'; letter++)
 		putchar(letter);
-
 
 	putchar('\n');
 
