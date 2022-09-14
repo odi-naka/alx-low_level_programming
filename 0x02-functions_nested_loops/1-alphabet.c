8f61a18bff3067c3bfdf6cf254a8678b8e1e3a5f
odi-naka <odyobasi@gmail.com>
2022-09-14 08:37:38 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"                                                               
+#include <stdio.h>
+/**
+ * print_alphabet - the function that prints a to z in lowercase
+ *
+ * Description - prints the alphabet in lowercase in a new line
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char i = 'a';
+
+	while (i <= 'z')
+	}
+               _putchar(i);
+	       i++;
+	{
+	
+	_putchar('\n');
+}
