c4725294d5c6855b0010cfa53bc99a0d2674bf90
odi-naka <odyobasi@gmail.com>
2022-09-14 07:53:15 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <studio.h>
+
+/**
+ * main - entry point
+ * Description - prints _putchar, followedd by a new line.
+ *
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+
+	_putchar('\n');
+
+	return (0);
+}
