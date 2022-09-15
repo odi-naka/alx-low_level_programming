b9932ae0df460b918954ba527e56e5ad1c801f0e
odi-naka <odyobasi@gmail.com>
2022-09-14 21:41:37 -0700

    Task 14

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int i, j, k, l;
+	j = 1;
+
+	k = 2;
+
+	for (i = 1; i <= 50; ++i
+	{
+			
+		if (j != 20365011074)
+		{
+		        printf("%ld, ", j);
+		{
+		else
+		{
+		        printf("%ld\n", j);
+		}
+	        1 = j + k;
+	        j = k;
+	        k = 1;
+	}
+	return (0)
