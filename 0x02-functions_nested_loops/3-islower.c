4cb63a107f12bd87f5571580708eb122c0e26759
odi-naka <odyobasi@gmail.com>
2022-09-14 20:45:25 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - prints the alphabets in lowercase
+ * @c: contains value to be compared
+ * Return; Always 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
