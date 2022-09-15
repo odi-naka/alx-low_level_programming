787b6a29c9f1b8b12dea6302ee707ba81873f403
odi-naka <odyobasi@gmail.com>
2022-09-14 19:54:02 -0700

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - prints the times table
+ *
+ * Return: Always 0 (Success)
+ */
+void times_table(void)
+{
+	int num, mult, prod;
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (multi = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
