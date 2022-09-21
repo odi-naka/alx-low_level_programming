d7b86193286133df48c62745d31637c1945777a3
odi-naka <odyobasi@gmail.com>
2022-09-21 09:53:39 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -11,12 +11,12 @@
 
 void _puts(char *str)
 {
-	int i = 0
+	int i = 0;
 	
-	while (*(str + i) != '\0')
+	while (str[i])
 	{
-		putchar(*(str + i));
+		putchar(str[i]);
 		i++;
 	}
-	putchar(10);
+	_putchar('\n');
 }
