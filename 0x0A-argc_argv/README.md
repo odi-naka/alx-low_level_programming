38c95b97f1924e705e125ee906bbed03c6e80347
odi-naka <odyobasi@gmail.com>
2022-09-30 08:35:23 -0700

    Task 0

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+utilizing arguments passed to C functions with argc and argv
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef _MAIN_H_INCLUDED
+#define _MAIN_H_INCLUDED
+
+int _putchar(char c);
+int check_num(char *str);
+
+#endif /*MAIN_H*/
